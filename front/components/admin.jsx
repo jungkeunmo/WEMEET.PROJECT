5410a30ffcafcf722f073fb280a34a19c0e3c3f5
jungkeunmo <abc59684495@gmail.com>
2023-04-15 22:04:22 +0900

    admin

---
+++ front/components/admin.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import {} from "antd";
+import {} from "react-cookie";
+
+const AppShell = ({}) => {
+  return (
+    <>
+      <text>ADMIN</text>
+    </>
+  );
+};
+
+export default AppShell;
